c3df8c0506f38b4a7728cf2b921970effe3f19d0
Aleja Muñoz <mamunozgo@unal.edu.co>
2025-11-30 22:43:39 -0500

    Solucion calculadora

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,5 +1,38 @@
 const pantalla = document.querySelector(".pantalla");
 const botones = document.querySelectorAll(".btn");
 
-//Tú código va acá
-//hint: Busca sobre la función eval
+botones.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    const botonApretado = boton.textContent;
+
+    if (boton.id === "c") {
+      // Botón C - Limpiar todo y poner en 0
+      pantalla.textContent = "0";
+      return;
+    }
+
+    if (boton.id === "borrar") {
+      if (pantalla.textContent.length === 1 || pantalla.textContent === "Error!") {
+        pantalla.textContent = "0";
+      } else {
+        pantalla.textContent = pantalla.textContent.slice(0, -1);
+      }
+      return;
+    }
+
+    if (boton.id === "igual") {
+      try {
+        pantalla.textContent = eval(pantalla.textContent);
+      } catch (error) {
+        pantalla.textContent = "Error";
+      }
+      return;
+    }
+
+    if (pantalla.textContent === "0" || pantalla.textContent === "Error") {
+      pantalla.textContent = botonApretado;
+    } else {
+      pantalla.textContent += botonApretado;
+    }
+  });
+});
